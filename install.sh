cb0b04c072c8918146937690cd967ab869f82601
Doering Christian <christian.doering@tum.de>
2023-02-18 01:22:50 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+cd $HOME
+
+NERD_FONT=DroidSansMono
+
+echo "Installing Development Tools..."
+sudo dnf groupinstall -y "Development Tools" "Development Libraries"
+sudo dnf install -y cmake
+sudo dnf install -y wget
+sudo dnf install -y util-linux-user
+sudo dnf install -y which
+
+echo "Installing Rust..."
+curl https://sh.rustup.rs -sSf | bash -s -- -y
+source $HOME/.bashrc
+
+echo "Installing ZSH..."
+dnf install -y zsh
+
+echo "Installing Nerd Font ($NERD_FONT)..."
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$NERD_FONT.zip
+mkdir .fonts
+unzip $NERD_FONT -d .fonts/
+rm -f $NERD_FONT.zip
+
+echo "Changing Default Shell to ZSH..."
+chsh -s $(which zsh)
+
+echo "Installing Oh-My-ZSH..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+exit
+
+echo "Installing Starship..."
+cargo install starship --locked
+echo 'eval "$(starship init zsh)"' >> .zshrc
