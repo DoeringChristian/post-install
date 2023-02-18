4aa7e5d40a36babcf5ef7ffd4cfced2f33189104
Doering Christian <christian.doering@tum.de>
2023-02-18 01:31:46 +0100

    Neovim install

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ sudo dnf install -y cmake
 sudo dnf install -y wget
 sudo dnf install -y util-linux-user
 sudo dnf install -y which
+sudo dnf install -y 'dnf-command(copr)'
 
 echo "Installing Rust..."
 curl https://sh.rustup.rs -sSf | bash -s -- -y
@@ -23,14 +24,18 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$NERD_FONT
 mkdir .fonts
 unzip $NERD_FONT -d .fonts/
 rm -f $NERD_FONT.zip
+fc-cache -fv
 
 echo "Changing Default Shell to ZSH..."
 chsh -s $(which zsh)
 
 echo "Installing Oh-My-ZSH..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-exit
 
 echo "Installing Starship..."
 cargo install starship --locked
 echo 'eval "$(starship init zsh)"' >> .zshrc
+
+echo "Installing NeoVim..."
+dnf copr enable -y agriffis/neovim-nightly
+dnf install -y neovim python3-neovim
